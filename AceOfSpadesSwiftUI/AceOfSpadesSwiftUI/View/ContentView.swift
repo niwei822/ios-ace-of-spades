d266e82d6700ca4e6c224a3c6fa6e7cdee68da96
Niwei Li <cecily.li.822@gmail.com>
2022-07-05 20:38:23 -0700

    ios-ace

--- AceOfSpadesSwiftUI/AceOfSpadesSwiftUI/View/ContentView.swift
+++ AceOfSpadesSwiftUI/AceOfSpadesSwiftUI/View/ContentView.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello, World!")
-  }
+    
+    var body: some View {
+        
+        ZStack{
+            LinearGradient(colors: [.green], startPoint: .bottom, endPoint: .top)
+                .edgesIgnoringSafeArea(.all)
+            Rectangle().foregroundColor(.white)
+                .frame(width: 300, height: 450, alignment: .center)
+                .cornerRadius(20)
+            
+            Image("Spade")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100, alignment: .center)
+            
+            Image("Spade")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+                .offset(x: -115, y: -160)
+            
+            Text("A")
+                .font(.system(size: 40, weight: .light, design: .serif))
+                .offset(x: -115, y: -190)
+            
+            Image("Spade")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25, height: 25)
+                .offset(x: -115, y: -160)
+                .rotationEffect(.degrees(180))
+            
+            Text("A")
+                .font(.system(size: 40, weight: .light, design: .serif))
+                .offset(x: -115, y: -190)
+                .rotationEffect(.degrees(180))
+        }
+    }
 }
-
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
